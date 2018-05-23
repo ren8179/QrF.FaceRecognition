be483d670e75cdd9a87708c46ec9b9d58dc396eb
RenQi <ren8179@126.com>
2018-05-23 13:49:09 +0800

    添加视频人脸识别功能

---
+++ src/QrF.FaceRecForCamera/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace QrF.FaceRecForCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(555, 426);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 223);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 42;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(573, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 426);
+            this.groupBox1.TabIndex = 43;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作面板";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "打开摄像头";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "关闭摄像头";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(70, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 45;
+            this.button3.Text = "拍照";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/QrF.FaceRecForCamera/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QrF.FaceRecForCamera
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _cameraCapture;
+
+        CascadeClassifier face = new CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml");
+        CascadeClassifier eye = new CascadeClassifier("haarcascades/haarcascade_eye.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _cameraCapture = new VideoCapture(1);
+            Application.Idle += ProcessFrame;
+            if (_cameraCapture != null)
+                _cameraCapture.Start();//摄像头开启
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_cameraCapture != null && _cameraCapture.Ptr != IntPtr.Zero)
+            {
+                var _frame = _cameraCapture.QueryFrame();
+                //_cameraCapture.Retrieve(_frame, cbxCameras.SelectedIndex);
+                if (_frame != null)
+                {
+                    using (UMat ugray = new UMat())
+                    {
+                        CvInvoke.CvtColor(_frame, ugray, ColorConversion.Bgr2Gray);
+                        CvInvoke.EqualizeHist(ugray, ugray);
+                        Rectangle[] facesDetected = face.DetectMultiScale(
+                             ugray,
+                             1.1,
+                             10,
+                             new Size(20, 20));
+                        foreach (Rectangle face in facesDetected)
+                        {
+                            //Get the region of interest on the faces
+                            using (UMat faceRegion = new UMat(ugray, face))
+                            {
+                                Rectangle[] eyesDetected = eye.DetectMultiScale(
+                                   faceRegion,
+                                   1.1,
+                                   10,
+                                   new Size(20, 20));
+
+                                foreach (Rectangle eye in eyesDetected)
+                                {
+                                    Rectangle eyeRect = eye;
+                                    eyeRect.Offset(face.X, face.Y);
+                                    CvInvoke.Rectangle(_frame, eyeRect, new Bgr(Color.Blue).MCvScalar, 2);
+                                }
+                            }
+                            CvInvoke.Rectangle(_frame, face, new Bgr(Color.Red).MCvScalar, 2);
+                        }
+                    }
+                }
+                imageBox1.Image = _frame;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _cameraCapture.Stop();//摄像头关闭  
+            _cameraCapture.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = _cameraCapture.QueryFrame().Bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_cameraCapture != null)
+            {
+                _cameraCapture.Stop();//摄像头关闭  
+                _cameraCapture.Dispose();
+            }
+        }
+    }
+}
